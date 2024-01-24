a5edd240586ec1d6ab7000ac0079b1c8424e50f6
Hari Om Swarup S A <84673594+NyanCyanide@users.noreply.github.com>
2024-01-25 00:57:28 +0530

    Initial Commit
    Simple Implementation of Fruit Ninja Game - yet to add Sound and more....

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI; 
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public Image fadeImage;
+    private int score;
+
+    private Blade blade;
+    private Spawner spawner;
+    private void Awake()
+    {
+        blade = FindObjectOfType<Blade>();
+        spawner = FindObjectOfType<Spawner>();
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+
+        Time.timeScale = 1f;
+        blade.enabled = true;
+        spawner.enabled = true;
+
+
+        score = 0;
+        scoreText.text = "Score : " + score.ToString();
+
+        ClearScene();
+    }
+
+    private void ClearScene() {
+        Fruit[] fruits = FindObjectsOfType<Fruit>();
+        foreach (Fruit fruit in fruits) {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsOfType<Bomb>();
+        foreach (Bomb bomb in bombs) {
+            Destroy(bomb.gameObject);
+        }
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score : " + score.ToString();
+    }
+
+    public void Explode() {
+        blade.enabled = false;
+        spawner.enabled = false;
+
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence() {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration) {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
+
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        NewGame();
+        elapsed = 0f;
+        while (elapsed < duration) {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.white, Color.clear, t);
+
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+    }
+}
